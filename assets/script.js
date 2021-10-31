d280ec8150b8550320d912aec6b3ba449251bab4
Zahra Mammadli <zahra.mrj@gmail.com>
2021-10-31 17:53:28 +0100

    getting 5 days forecast

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,138 @@
-// Storing city searched into the local storage
+// Set global variables, including Open Weather Maps API Key
+var owmAPI = "66535f8ed119f4412dae494719eaa500";
+var currentCity = "";
+var lastCity = "";
 
-$("#search-button").on("click", function (event) {
-  event.preventDefault();
-  var city = $(event.target).siblings().val();
-  console.log(city);
-  localStorage.setItem("city", city);
-  //   var tasks = $(event.target).parent().siblings().last();
-  //   var hour = $(event.target).parent().siblings().first();
-  //   localStorage.setItem(hour.text().trim(), tasks.val());
-});
+var handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+// Get and display the current conditions
+var getCurrentConditions = (event) => {
+  let city = $("#search-city").val();
+  currentCity = $("#search-city").val();
+  let queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial" +
+    "&APPID=" +
+    owmAPI;
+  fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      saveCity(city);
+      $("#search-error").text("");
+      let currentWeatherIcon =
+        "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+      let currentTimeUTC = response.dt;
+      let currentTimeZoneOffset = response.timezone;
+      let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+      let currentMoment = moment
+        .unix(currentTimeUTC)
+        .utc()
+        .utcOffset(currentTimeZoneOffsetHours);
+      renderCities();
+      getFiveDayForecast(event);
+      $("#header-text").text(response.name);
+      let currentWeatherHTML = `
+            <h3>${response.name} ${currentMoment.format(
+        "(MM/DD/YY)"
+      )}<img src="${currentWeatherIcon}"></h3>
+            <ul class="list-unstyled">
+                <li>Temperature: ${response.main.temp}&#8457;</li>
+                <li>Humidity: ${response.main.humidity}%</li>
+                <li>Wind Speed: ${response.wind.speed} mph</li>
+                <li id="uvIndex">UV Index:</li>
+            </ul>`;
+      // Append results to the DOM
+      $("#current-weather").html(currentWeatherHTML);
+      let latitude = response.coord.lat;
+      let longitude = response.coord.lon;
+      let uvQueryURL =
+        "api.openweathermap.org/data/2.5/uvi?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&APPID=" +
+        owmAPI;
+      uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+      fetch(uvQueryURL)
+        .then(handleErrors)
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          let uvIndex = response.value;
+          $("#uvIndex").html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+          if (uvIndex >= 0 && uvIndex < 3) {
+            $("#uvVal").attr("class", "uv-favorable");
+          } else if (uvIndex >= 3 && uvIndex < 8) {
+            $("#uvVal").attr("class", "uv-moderate");
+          } else if (uvIndex >= 8) {
+            $("#uvVal").attr("class", "uv-severe");
+          }
+        });
+    });
+};
+
+// get five days forecats
+var getFiveDayForecast = (event) => {
+  let city = $("#search-city").val();
+  let queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial" +
+    "&APPID=" +
+    owmAPI;
+  // Fetch from API
+  fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      let fiveDayForecastHTML = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
+      for (let i = 0; i < response.list.length; i++) {
+        let dayData = response.list[i];
+        let dayTimeUTC = dayData.dt;
+        let timeZoneOffset = response.city.timezone;
+        let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+        let thisMoment = moment
+          .unix(dayTimeUTC)
+          .utc()
+          .utcOffset(timeZoneOffsetHours);
+        let iconURL =
+          "https://openweathermap.org/img/w/" +
+          dayData.weather[0].icon +
+          ".png";
+        if (
+          thisMoment.format("HH:mm:ss") === "11:00:00" ||
+          thisMoment.format("HH:mm:ss") === "12:00:00" ||
+          thisMoment.format("HH:mm:ss") === "13:00:00"
+        ) {
+          fiveDayForecastHTML += `
+                <div class="weather-card card m-2 p0">
+                    <ul class="list-unstyled p-3">
+                        <li>${thisMoment.format("MM/DD/YY")}</li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
+                        <li>Temp: ${dayData.main.temp}&#8457;</li>
+                        <br>
+                        <li>Humidity: ${dayData.main.humidity}%</li>
+                    </ul>
+                </div>`;
+        }
+      }
+      fiveDayForecastHTML += `</div>`;
+      $("#five-day-forecast").html(fiveDayForecastHTML);
+    });
+};
+
+//  save the city to localStorage
